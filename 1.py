812c4d73db35ec7de8d0d886ebc885d556607a14
Olena Shatalova <olenashatalova1@gmail.com>
2026-03-04 22:40:37 +0200

    Homework 03 / 2.0

--- 1.py
+++ 1.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 
 def get_days_from_today(date: str) -> int:
- try:
-  input_date = datetime.strptime(date, "%Y-%m-%d").date()
-  today = datetime.today().date()
-  delta = today - input_date
- except ValueError:
-  print(f"{date} не відповідає формату 'YYYY-MM-DD'")
-  return None
- else:
-  return delta.days
+    try:
+        input_date = datetime.strptime(date, "%Y-%m-%d").date()
+        today = datetime.today().date()
+        delta = today - input_date
+    except ValueError:
+        print(f"{date} не відповідає формату 'YYYY-MM-DD'")
+        return None
+    else:
+        return delta.days
 
 # print(get_days_from_today("2026/2/28"))
 # print(get_days_from_today("2026-3-21"))
--- 4.py
+++ 4.py
@@ -41,8 +41,6 @@ def get_upcoming_birthdays(users: list) -> list:
 
     return upcoming
 
-
-
 # users = [
 #     {"name": "John Doe", "birthday": "1985.01.23"},
 #     {"name": "Jane Smith", "birthday": "1990.01.27"},
